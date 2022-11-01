46d490c796382f11a04a58d61130ea3bddf0d067
Suraj Devraye <surajdevraye@gmail.com>
2022-11-01 18:54:08 +0530

    accordion done

---
+++ src/AccordianList.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { slide } from "svelte/transition";
+  export let entry;
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="list-label" on:click={toggle} aria-expanded={isOpen}>
+  <span style="backgound-color: red">{entry[0]}</span>
+  <i class="expand-list-icon">
+    <svg
+      style="tran"
+      width="15"
+      height="15"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="3"
+      viewBox="0 0 24 24"
+      stroke="currentColor"><path d="M9 5l7 7-7 7" /></svg
+    >
+  </i>
+</div>
+{#if isOpen}
+  <ul transition:slide={{ duration: 300 }}>
+    {#each entry[1] as item}
+      <li>{item}</li>
+    {/each}
+  </ul>
+{/if}
+
+<style>
+  * {
+    padding: 0;
+  }
+
+ 
+
+  ul {
+    list-style: none;
+    background-color: #fff;
+  }
+  ul li {
+    padding: 2px;
+    background-color: bisque;
+    width: fit-content;
+  }
+  svg {
+    transition: transform 0.2s ease-in;
+  }
+
+  .expand-list-icon {
+    margin-top: 1px;
+    float: right;
+  }
+
+  [aria-expanded="true"] svg {
+    transform: rotate(0.25turn);
+  }
+</style>
--- src/Component.svelte
+++ src/Component.svelte
@@ -1,52 +1,156 @@
 <script>
-  import { getContext } from "svelte"
+  import { getContext } from "svelte";
+  import Icon from "svelte-icon";
 
-  export let option1
-  export let option2
-  export let option3
-  export let option4
-  export let option5
-  export let option6
-  export let option7
+  import costSummeryIcon from "./resources/cost-summary-dollar.svg";
+  import projectLibraryIcon from "./resources/cost-library.svg";
+  import reportIcon from "./resources/report.svg";
+  // import projectDetailsIcon from "./resources/details.htm";
+  import projectUsers from "./resources/users.svg";
 
-  const { styleable } = getContext("sdk")
-  const component = getContext("component")
+  import dropdownIcon from "./resources/dropdown-icon.svg";
+
+  import { slide } from "svelte/transition";
+  import AccordianList from "./AccordianList.svelte";
+
+  export let backgroundColor;
+  export let option1, option2, option3, option4, option5;
+
+  const { styleable } = getContext("sdk");
+  const component = getContext("component");
+
+  const subListData = {
+    Estimation: [
+      "Estimation Breakup",
+      "Estimation Summery",
+      "Estimation Revision",
+    ],
+    Costing: ["Cost Variation", "Cost Breakup"],
+    Resources: [
+      "Material",
+      "Labour",
+      "Machinery & Equipments",
+      "Material + Labour",
+    ],
+  };
+
+  const listData = ["Estimation", "Costing", "Resources"];
+
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
 </script>
 
 <div use:styleable={$component.styles} class="side-bar">
+  <div class="side-bar" style="--bg-color: {backgroundColor}">
+    <div class="list-style">
+      <ul>
+        <li>
+          <span class="list-icon">
+            <Icon data={costSummeryIcon} />
+          </span>{option1}
+        </li>
+        <li>
+          <span class="list-icon">
+            <Icon data={projectLibraryIcon} />
+          </span>{option2}
+        </li>
+        <li class="expand-list" >
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <div on:click={toggle} aria-expanded={isOpen}>
+          <span class="list-icon">
+            <Icon data={reportIcon} />
+          </span>{option3}
+          <i class="expand-list-icon">
+            <svg
+            style="tran"
+            width="15"
+            height="15"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="3"
+            viewBox="0 0 24 24"
+            stroke="currentColor"><path d="M9 5l7 7-7 7" /></svg
+          >
+          </i>
+        </div>
+          {#if isOpen}
 
-  <div class="side-bar">
-  <li class="list-style">
-    <ul class="list-item">{option1}</ul>
-    <ul class="list-item">{option2}</ul>
-    <ul class="list-item">{option3}</ul>
-    <ul class="list-item">{option4}</ul>
-    <ul class="list-item">{option5}</ul>
-    <ul class="list-item">{option6}</ul>
-    <ul class="list-item">{option7}</ul>
-  </li>
+          <div class="expanded-list" transition:slide={{ duration: 300 }}>
+            <ul>
+              {#each Object.entries(subListData) as entry}
+                <AccordianList {entry} />
+              {/each}
+
+            </ul>
+          </div>
+          {/if}
+
+        </li>
+        <li>
+          <span class="list-icon">
+            <Icon data={costSummeryIcon} />
+          </span>{option4}
+        </li>
+        <li>
+          <span class="list-icon">
+            <Icon data={projectUsers} />
+          </span>{option5}
+        </li>
+      </ul>
+    </div>
   </div>
 </div>
 
-
 <style>
-  div{
+  * {
     padding: 0;
     margin: 0;
   }
-  .side-bar{
+  span {
+    vertical-align: middle;
+  }
+  .side-bar {
     width: max-content;
     height: 100%;
-    background-color: white;
+    background-color: var(--bg-color, rgba(255, 255, 255, 0));
   }
 
-  .list-style{
-    list-style: none;
+  .list-style {
     padding: 10px;
   }
+  .list-style ul {
+    list-style: none;
+  }
+  .list-style ul li {
+    padding: 5px 10px;
+  }
+  .list-style ul li:hover {
+    background-color: rgba(0, 100, 255, 0.1);
+    cursor: pointer;
+  }
+  .list-icon {
+    margin-right: 10px;
+  }
+  .expand-list-icon {
+    float: right;
+    margin-top: 5px;
+  }
 
-  .list-item{
-    padding: 5px;
-    margin: 3px;
+
+
+  .expand-list:hover .list-icon {
+    transform: rotate(-90deg);
+    -webkit-transform: rotate(-90deg);
+  }
+
+  .expand-list-icon {
+    margin-top: 2px;
+    float: right;
+    
+  }
+
+  [aria-expanded="true"] svg {
+    transform: rotate(0.25turn);
   }
 </style>
